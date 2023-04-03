bbf8947210e0aba96abbe8f6264a908c4b07bff9
Wrab <namedj82@gmail.com>
2023-04-03 11:49:27 +0300

    config and autostart files

---
+++ autostart.sh
@@ -0,0 +1,8 @@
+numlockx &
+xkbset bo 20 &
+dwmblocks &
+nitrogen --restore &
+dunst -conf ~/.config/dunstrc &
+copyq &
+setxkbmap -layout "us,ru" -option grp:alt_shift_toggle &
+x0vncserver --localhost -display :0 -SecurityTypes=none
